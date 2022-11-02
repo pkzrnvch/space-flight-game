af1fab0313ee3bd2efa65ecf875127cdefc7c98d
pkzrnvch <pkazarnovich@gmail.com>
2022-11-02 09:53:58 +0500

    Add rocket animation

--- main.py
+++ main.py
@@ -2,9 +2,51 @@ import asyncio
 import time
 import curses
 from random import randint, choice
+from itertools import cycle
 
 
 TIC_TIMEOUT = 0.1
+
+
+def read_frame_file(file_name):
+    """Read frame file"""
+
+    with open(f'frames/{file_name}', 'r') as frame_file:
+        frame = frame_file.read()
+    return frame
+
+
+def draw_frame(canvas, start_row, start_column, text, negative=False):
+    """Draw multiline text fragment on canvas, erase text instead of drawing if negative=True is specified."""
+
+    rows_number, columns_number = canvas.getmaxyx()
+
+    for row, line in enumerate(text.splitlines(), round(start_row)):
+        if row < 0:
+            continue
+
+        if row >= rows_number:
+            break
+
+        for column, symbol in enumerate(line, round(start_column)):
+            if column < 0:
+                continue
+
+            if column >= columns_number:
+                break
+
+            if symbol == ' ':
+                continue
+
+            # Check that current position is not in a lower right corner
+            # of the window.
+            # Curses will raise exception in that case. Don`t ask why…
+            # https://docs.python.org/3/library/curses.html#curses.window.addch
+            if row == rows_number - 1 and column == columns_number - 1:
+                continue
+
+            symbol = symbol if not negative else ' '
+            canvas.addch(row, column, symbol)
 
 
 async def blink(canvas, row, column, symbol='*'):
@@ -57,11 +99,21 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
+async def display_rocket(canvas, raw, column, rocket_frames):
+    for frame in cycle(rocket_frames):
+        draw_frame(canvas, raw, column, frame)
+        await asyncio.sleep(0)
+        draw_frame(canvas, raw, column, frame, negative=True)
+
+
 def draw(canvas):
     canvas.border()
     rows, columns = canvas.getmaxyx()
     max_height, max_width = rows - 2, columns - 2
     curses.curs_set(False)
+    rocket_frame_1 = read_frame_file('rocket_frame_1.txt')
+    rocket_frame_2 = read_frame_file('rocket_frame_2.txt')
+    rocket_frames = [rocket_frame_1, rocket_frame_2]
     star_coordinates = [
         (randint(1, max_height), randint(1, max_width))
         for _ in range(100)
@@ -72,6 +124,7 @@ def draw(canvas):
         for coordinate in star_coordinates
     ]
     coroutines.append(fire(canvas, 15, 50))
+    coroutines.append(display_rocket(canvas, 20, 20, rocket_frames))
     while True:
         for coroutine in coroutines:
             coroutine.send(None)
