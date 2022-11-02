1aa448e8502e5cbe7ce25d109cbd52b79b1791ce
pkzrnvch <pkazarnovich@gmail.com>
2022-11-02 10:50:05 +0500

    Enable rocket movement with control keys

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from itertools import cycle
 
 
 TIC_TIMEOUT = 0.1
+SPACE_KEY_CODE = 32
+LEFT_KEY_CODE = 260
+RIGHT_KEY_CODE = 261
+UP_KEY_CODE = 259
+DOWN_KEY_CODE = 258
 
 
 def read_frame_file(file_name):
@@ -47,6 +52,37 @@ def draw_frame(canvas, start_row, start_column, text, negative=False):
 
             symbol = symbol if not negative else ' '
             canvas.addch(row, column, symbol)
+
+
+def read_controls(canvas):
+    """Read keys pressed and returns tuple witl controls state."""
+
+    rows_direction = columns_direction = 0
+    space_pressed = False
+
+    while True:
+        pressed_key_code = canvas.getch()
+
+        if pressed_key_code == -1:
+            # https://docs.python.org/3/library/curses.html#curses.window.getch
+            break
+
+        if pressed_key_code == UP_KEY_CODE:
+            rows_direction = -1
+
+        if pressed_key_code == DOWN_KEY_CODE:
+            rows_direction = 1
+
+        if pressed_key_code == RIGHT_KEY_CODE:
+            columns_direction = 1
+
+        if pressed_key_code == LEFT_KEY_CODE:
+            columns_direction = -1
+
+        if pressed_key_code == SPACE_KEY_CODE:
+            space_pressed = True
+
+    return rows_direction, columns_direction, space_pressed
 
 
 async def blink(canvas, row, column, symbol='*'):
@@ -99,11 +135,19 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
-async def display_rocket(canvas, raw, column, rocket_frames):
+async def display_rocket(canvas, rocket_frames, raw, column):
     for frame in cycle(rocket_frames):
         draw_frame(canvas, raw, column, frame)
         await asyncio.sleep(0)
+        raw_direction, column_direction, _ = read_controls(canvas)
+        next_raw = raw + raw_direction
+        next_column = column + column_direction
+        await asyncio.sleep(0)
+        raw_direction, column_direction, _ = read_controls(canvas)
+        next_raw += raw_direction
+        next_column += column_direction
         draw_frame(canvas, raw, column, frame, negative=True)
+        raw, column = next_raw, next_column
 
 
 def draw(canvas):
@@ -111,6 +155,7 @@ def draw(canvas):
     rows, columns = canvas.getmaxyx()
     max_height, max_width = rows - 2, columns - 2
     curses.curs_set(False)
+    canvas.nodelay(True)
     rocket_frame_1 = read_frame_file('rocket_frame_1.txt')
     rocket_frame_2 = read_frame_file('rocket_frame_2.txt')
     rocket_frames = [rocket_frame_1, rocket_frame_2]
@@ -123,12 +168,14 @@ def draw(canvas):
         blink(canvas, coordinate[0], coordinate[1], choice(star_symbols))
         for coordinate in star_coordinates
     ]
-    coroutines.append(fire(canvas, 15, 50))
-    coroutines.append(display_rocket(canvas, 20, 20, rocket_frames))
+    coroutines.append(display_rocket(canvas, rocket_frames, 20, 20))
     while True:
-        for coroutine in coroutines:
-            coroutine.send(None)
-            canvas.refresh()
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+                canvas.refresh()
+            except StopIteration:
+                coroutines.remove(coroutine)
         time.sleep(TIC_TIMEOUT)
 
 
