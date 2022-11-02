176815685b6cf792b07cc00ca1766dd645614954
pkzrnvch <pkazarnovich@gmail.com>
2022-11-02 16:12:09 +0500

    Limit rocket movement area within canvas borders

--- main.py
+++ main.py
@@ -54,8 +54,17 @@ def draw_frame(canvas, start_row, start_column, text, negative=False):
             canvas.addch(row, column, symbol)
 
 
+def get_frame_size(text):
+    """Calculate size of multiline text fragment, return pair — number of rows and columns."""
+
+    lines = text.splitlines()
+    rows = len(lines)
+    columns = max([len(line) for line in lines])
+    return rows, columns
+
+
 def read_controls(canvas):
-    """Read keys pressed and returns tuple witl controls state."""
+    """Read keys pressed and returns tuple with controls state."""
 
     rows_direction = columns_direction = 0
     space_pressed = False
@@ -135,19 +144,33 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
-async def display_rocket(canvas, rocket_frames, raw, column):
+async def display_rocket(canvas, rocket_frames):
+    """Display rocket movement animation."""
+
+    max_row, max_column = canvas.getmaxyx()
+    max_frame_height = max([get_frame_size(frame)[0] for frame in rocket_frames])
+    max_frame_width = max([get_frame_size(frame)[1] for frame in rocket_frames])
+
+    row = round(max_row / 2 - max_frame_height / 2)
+    column = round(max_column / 2 - max_frame_width / 2)
+
     for frame in cycle(rocket_frames):
-        draw_frame(canvas, raw, column, frame)
+        draw_frame(canvas, row, column, frame)
         await asyncio.sleep(0)
-        raw_direction, column_direction, _ = read_controls(canvas)
-        next_raw = raw + raw_direction
+
+        row_direction, column_direction, _ = read_controls(canvas)
+        next_row = row + row_direction
         next_column = column + column_direction
         await asyncio.sleep(0)
-        raw_direction, column_direction, _ = read_controls(canvas)
-        next_raw += raw_direction
+
+        row_direction, column_direction, _ = read_controls(canvas)
+        next_row += row_direction
         next_column += column_direction
-        draw_frame(canvas, raw, column, frame, negative=True)
-        raw, column = next_raw, next_column
+
+        draw_frame(canvas, row, column, frame, negative=True)
+        if max_row - max_frame_height > next_row > 0 \
+                and max_column - max_frame_width > next_column > 0:
+            row, column = next_row, next_column
 
 
 def draw(canvas):
@@ -156,19 +179,23 @@ def draw(canvas):
     max_height, max_width = rows - 2, columns - 2
     curses.curs_set(False)
     canvas.nodelay(True)
+
     rocket_frame_1 = read_frame_file('rocket_frame_1.txt')
     rocket_frame_2 = read_frame_file('rocket_frame_2.txt')
     rocket_frames = [rocket_frame_1, rocket_frame_2]
+
     star_coordinates = [
         (randint(1, max_height), randint(1, max_width))
         for _ in range(100)
     ]
     star_symbols = '+*:.'
+
     coroutines = [
         blink(canvas, coordinate[0], coordinate[1], choice(star_symbols))
         for coordinate in star_coordinates
     ]
-    coroutines.append(display_rocket(canvas, rocket_frames, 20, 20))
+    coroutines.append(display_rocket(canvas, rocket_frames))
+
     while True:
         for coroutine in coroutines.copy():
             try:
